e1463995abc96feb1bfb04657f7429aa99337427
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-10-04 14:37:54 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h3>Hello World</h3>
       </header>
     </div>
   );
