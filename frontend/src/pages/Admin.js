d1a17987c4494bfe20e02726f92650cb0e89f65b
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-12-09 23:25:47 +0100

    Upload page css

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Usernav from "../components/Usernav";
 import { Outlet } from "react-router-dom";
-import "./Admin.css"
+import "./Admin.css";
 
 const Admin = () => {
   return (
     <div>
-      <Usernav />
-      <div className="main_section">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 };
--- frontend/src/pages/UploadProject.js
+++ frontend/src/pages/UploadProject.js
@@ -3,14 +3,12 @@ import { useForm } from "react-hook-form";
 import { useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-
+import "./UploadProject.css";
 
 const UploadProject = () => {
   const { register, handleSubmit } = useForm();
   const [images, setImages] = useState([]);
-  const { userInfo } = useSelector(
-    (state) => state.auth
-  );
+  const { userInfo } = useSelector((state) => state.auth);
   const onChangeImgs = (e) => {
     e.preventDefault();
     setImages(e.target.files);
@@ -19,10 +17,10 @@ const UploadProject = () => {
     const form = new FormData();
     Object.keys(data).map((key, index) => {
       form.append(key, data[key]);
-      console.log("KEY : " , key, data[key]);
+      console.log("KEY : ", key, data[key]);
     });
     //get owner from redux store
-    form.append('owner' , userInfo.id )
+    form.append("owner", userInfo.id);
     console.log("IMGS Selected : ", images);
     for (let i = 0; i < images.length; i++) {
       form.append("images", images[i]);
@@ -35,79 +33,101 @@ const UploadProject = () => {
   };
 
   return (
-    <div className="upload_project_section">
-      <h1 className="section_title">PROJECT DASH</h1>
+    <div className="upload_project_section container">
+      <div className="row">
+        <h1 className="section_title ">
+          PROJECT
+          <br />
+          DASH
+        </h1>
+      </div>
       <form
         noValidate
         onSubmit={handleSubmit(submitForm)}
-        encType="multipart/form-data"
+        encType="multipart/form-data row container"
       >
-        <div className="upload_project_input" id="title_div">
-          <label htmlFor="title">CARD TITLE</label>
-          <input
-            type="text"
-            placeholder="name"
-            {...register("title")}
-            required
-            id="title"
-          />
-        </div>
-        <div className="upload_project_input" id="type_div">
-          <label htmlFor="type">CARD TYPE</label>
-          <select
-            placeholder="type"
-            name="type"
-            id="type"
-            {...register("cardType", {
-              valueAsNumber: true,
-            })}
+        <div className="row">
+          <div
+            className="upload_project_input col d-flex flex-column"
+            id="title_div"
           >
-            <option value="0" defaultValue selected disabled>
-              type
-            </option>
-            <option value="0">0</option>
-            <option value="1">1</option>
-          </select>
-        </div>
-
-        <div className="upload_project_input" id="imgs_div">
-          <label htmlFor="images">IMG</label>
-          <input
-            type="file"
-            id="images"
-            name="images"
-            multiple
-            placeholder="import"
-            {...register("images")}
-            onChange={onChangeImgs}
-          />
-        </div>
-        <div className="upload_project_input" id="gradient_div">
-          <label htmlFor="hexs">GRADIENT</label>
-          <div id="hexs">
-            <input type="color" id="hex1" {...register("hex1")} />
-            <input type="color" id="hex2" {...register("hex2")} />
+            <label htmlFor="title">CARD TITLE</label>
+            <input
+              type="text"
+              placeholder="name"
+              {...register("title")}
+              required
+              id="title"
+            />
+          </div>
+          <div
+            className="upload_project_input col d-flex flex-column"
+            id="type_div"
+          >
+            <label htmlFor="type">CARD TYPE</label>
+            <select
+              placeholder="type"
+              name="type"
+              id="type"
+              {...register("cardType", {
+                valueAsNumber: true,
+              })}
+            >
+              <option value="0" defaultValue selected disabled>
+                type
+              </option>
+              <option value="0">0</option>
+              <option value="1">1</option>
+            </select>
           </div>
         </div>
-
-        <div className="upload_project_input" id="parag1_div">
-          <label htmlFor="parag1">PARAGRAPH 1</label>
-          <textarea
-            type="text"
-            placeholder="Type your description here"
-            required
-            id="parag1"
-            {...register("parag1")}
-          />
+        <div className="row">
+          <div className="upload_project_input col" id="imgs_div">
+            <label htmlFor="images">IMG</label>
+            <input
+              type="file"
+              id="images"
+              name="images"
+              multiple
+              placeholder="import"
+              {...register("images")}
+              onChange={onChangeImgs}
+            />
+          </div>
+          <div className="upload_project_input col" id="gradient_div">
+            <label htmlFor="hexs">GRADIENT</label>
+            <div id="hexs">
+              <input type="color" id="hex1" {...register("hex1")} />
+              <input type="color" id="hex2" {...register("hex2")} />
+            </div>
+          </div>
         </div>
-        <div className="upload_project_input" id="parag2_div">
-          <label htmlFor="parag2">PARAGRAPH 2</label>
-          <textarea
-            type="text"
-            placeholder="Type your description here"
-            id="parag2"
-            {...register("parag2")}
-          />
+        <div className="row">
+          <div
+            className="upload_project_input col d-flex flex-column"
+            id="parag1_div"
+          >
+            <label htmlFor="parag1">PARAGRAPH 1</label>
+            <textarea
+              type="text"
+              placeholder="Type your description here"
+              required
+              id="parag1"
+              {...register("parag1")}
+            />
+          </div>
+          <div
+            className="upload_project_input col d-flex flex-column"
+            id="parag2_div"
+          >
+            <label htmlFor="parag2">PARAGRAPH 2</label>
+            <textarea
+              type="text"
+              placeholder="Type your description here"
+              id="parag2"
+              {...register("parag2")}
+            />
+          </div>
         </div>
         <button type="submit">Validate Project</button>
       </form>
