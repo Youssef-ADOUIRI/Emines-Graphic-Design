60249820b5bc4839c53a71e9a85a4a829b124b2e
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2023-08-30 13:36:56 +0100

    final commit before delete

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,5 +1,5 @@
 // Add your Mongodb URI here to connect to your database
-const MONGODB_URI = "mongodb+srv://"
+const MONGODB_URI = "mongodb+srv://admin_user:LelYQyQsrfXOrJDj@egd-cluster.si4tr9i.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = {
   mongoURI: MONGODB_URI,
